08fdd0b97b88ab6bc525fc530acf47fea02c22c4
Naveen Singh <naveensingh45bisht@gmail.com>
2025-09-27 11:18:03 +0530

    RW Implementation
    Implementation of Reader/Writer Problem

--- src/Client.java
+++ src/Client.java
@@ -1,19 +1,57 @@
 
-public class Client {
+public class Client 
+{
 	int id;
 	boolean isAlive;
+	boolean isWriter;
 	Server leader;
 	String content = "";
-	void requestLock() {
-		
+
+	public Client(int id, boolean isWriter, Server leader)
+	{
+        this.id = id;
+        this.isWriter = isWriter;
+        this.leader = leader;
+    }
+
+	void requestLock()
+	{	
+		if (isWriter) {
+            leader.acquireWriteLock(id);
+        } else {
+            leader.acquireReadLock(id);
+        }
 	}
-	void writeAndReleaseLock() {
-		
+	void writeAndReleaseLock() 
+	{
+		if (isWriter) {
+            leader.write(id, "Data from Client-" + id);
+            leader.releaseWriteLock(id);
+        }	
 	}
-	void readContent() {
-		
+	void readContent()
+	{
+		if (!isWriter) {
+            leader.read(id);
+            leader.releaseReadLock(id);
 	}
-	void simulateCrash() {
-		
+	void simulateCrash()
+	{
+		isAlive = false;
+        System.out.println("Client-" + id + " crashed!");
 	}
+
+	public void run()
+	{
+        if (isWriter)
+		{
+            requestLock();
+            writeAndReleaseLock();
+        }
+		else
+		{
+            requestLock();
+            readContent();
+        }
+    }
 }
